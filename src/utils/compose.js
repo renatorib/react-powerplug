c8eb734c7ebe33dd799eb494b5f802667b942205
Renato Ribeiro <renatoribroman@gmail.com>
2017-09-11 14:10:39 -0300

    feat(compose): fix grammar of comments

--- src/utils/compose.js
+++ src/utils/compose.js
@@ -2,17 +2,18 @@ import React from 'react'
 
 const compose = (...elements) => {
   return ({ children }) => {
-    // Recursively stack children arguments and pass
+
+    // Stack children arguments recursively and pass
     // it down until the last component that render children
     // with these stacked arguments
     function stackProps(i, elements, stacked = {}) {
-      // check if is latest component,
-      // if is latest render children,
-      // otherwise continue stacking arguments
-      const childrenFn = props =>
-        i === 0
-          ? children({ ...props, ...stacked }) 
-          : stackProps(i - 1, elements, { ...props, ...stacked })
+
+      // Check if is latest component.
+      // If is latest then render children,
+      // Otherwise continue stacking arguments
+      const childrenFn = props => i === 0
+        ? children({ ...props, ...stacked }) 
+        : stackProps(i - 1, elements, { ...props, ...stacked })
 
       return React.cloneElement(elements[i], {}, childrenFn)
     }
