860da2e6626550911e01035fb53d0bb6d72e1834
Renato Ribeiro <renatoribroman@gmail.com>
2017-10-18 11:49:53 -0200

    Consistent Counter children api

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,14 +2,18 @@ import React from 'react'
 import State from './State'
 import renderProps from '../utils/renderProps'
 
+const add = (value) => (prev) => ({
+  count: prev.count + value,
+})
+
 const Counter = ({ initial = 0, onChange, ...props }) => (
   <State initial={{ count: initial }} onChange={onChange}>
     {({ state, setState }) => renderProps(props, {
       count: state.count,
-      inc: (value = 1) =>
-        setState(s => ({ count: s.count + value })),
-      dec: (value = 1) =>
-        setState(s => ({ count: s.count - value })),
+      inc: () => setState(add(1)),
+      dec: () => setState(add(-1)),
+      incBy: (value) => setState(add(value)),
+      decBy: (value) => setState(add(-value)),
     })}
   </State>
 )
