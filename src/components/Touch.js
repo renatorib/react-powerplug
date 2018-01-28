dc26a45212b751e1026c22fcab3a31003d75af04
Farzad YZ <farskid@gmail.com>
2018-01-28 20:28:56 +0330

    Adds Touch component

---
+++ src/components/Touch.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import State from './State'
+import renderProps from '../utils/renderProps'
+
+const Touch = ({ onChange, ...props }) => (
+  <State initial={{ isTouched: false }} onChange={ onChange }>
+    {({ state, setState }) => renderProps(props, {
+      isTouched: state.isTouched,
+      bindTouch: {
+        onTouchStart: () => setState({ isTouched: true }),
+        onTouchEnd: () => setState({ isTouched: false }),
+      },
+    })}
+  </State>
+)
+
+export default Touch
