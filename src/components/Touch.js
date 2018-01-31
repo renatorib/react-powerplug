7e19ca2247ef96c4ee4a9d40a45078e439bfca92
Renato Ribeiro <renatoribroman@gmail.com>
2018-01-31 21:55:07 -0200

    Add Touch.md docs

--- src/components/Touch.js
+++ src/components/Touch.js
@@ -3,14 +3,16 @@ import State from './State'
 import renderProps from '../utils/renderProps'
 
 const Touch = ({ onChange, ...props }) => (
-  <State initial={{ isTouched: false }} onChange={ onChange }>
-    {({ state, setState }) => renderProps(props, {
-      isTouched: state.isTouched,
-      bind: {
-        onTouchStart: () => setState({ isTouched: true }),
-        onTouchEnd: () => setState({ isTouched: false }),
-      },
-    })}
+  <State initial={{ isTouched: false }} onChange={onChange}>
+    {({ state, setState }) =>
+      renderProps(props, {
+        isTouched: state.isTouched,
+        bind: {
+          onTouchStart: () => setState({ isTouched: true }),
+          onTouchEnd: () => setState({ isTouched: false }),
+        },
+      })
+    }
   </State>
 )
 
