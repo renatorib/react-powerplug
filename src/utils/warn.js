d76d94855327116ea037e2a7111db259ac593fd9
Bogdan Chadkin <trysound@yandex.ru>
2018-02-01 14:57:11 +0300

    Fixed usage of process.env.NODE_ENV (#49)

--- src/utils/warn.js
+++ src/utils/warn.js
@@ -1,11 +1,7 @@
 /* eslint-disable no-console */
 
-const hasEnv = process && process.env
-
-const isNotProd = hasEnv && process.env.NODE_ENV !== 'production'
-
 const warn = (condition, message, trace = true) => {
-  if (isNotProd && !!condition) {
+  if (!!condition) {
     console.warn(`[react-powerplug]: ${message}`)
     console.trace && trace && console.trace('Trace')
   }
