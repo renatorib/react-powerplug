2e45f70814209e66607c362dd1bdb058ab8c614e
renatorib <renatoribroman@gmail.com>
2018-02-08 17:55:08 -0200

    fix warn lint

--- src/utils/warn.js
+++ src/utils/warn.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 
 const warn = (condition, message, trace = true) => {
-  if (!!condition) {
+  if (condition) {
     console.warn(`[react-powerplug]: ${message}`)
     console.trace && trace && console.trace('Trace')
   }
