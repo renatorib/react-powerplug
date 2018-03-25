2afa42d41cade69cb5bfcb53a5998182db2e9eac
Bogdan Chadkin <trysound@yandex.ru>
2018-03-25 06:50:52 +0300

    Combine UMD configs into rollup multiconfig and add size snapshot (#73)
    * Combine UMD configs into rollup multiconfig
    
    * Add size-snapshot

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,54 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 import uglify from 'rollup-plugin-uglify'
 
-const env = process.env.NODE_ENV
+const getUMDConfig = ({ env, file }) => {
+  const config = {
+    input: 'src/index.js',
+    output: {
+      file,
+      format: 'umd',
+      name: 'ReactPowerPlug',
+      globals: {
+        react: 'React',
+      },
+    },
+    external: ['react'],
+    plugins: [
+      nodeResolve(),
+      babel({
+        runtimeHelpers: true,
+        exclude: '**/node_modules/**',
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(env),
+      }),
+    ],
+  }
 
-const config = {
-  input: 'src/index.js',
-  output: {
-    format: 'umd',
-    name: 'ReactPowerPlug',
-    globals: {
-      'react': 'React',
-    }
-  },
-  external: [
-    'react',
-  ],
-  plugins: [
-    nodeResolve(),
-    babel({
-      runtimeHelpers: true,
-      exclude: '**/node_modules/**'
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(env)
-    })
-  ]
+  if (env === 'development') {
+    config.plugins.push(sizeSnapshot())
+  }
+
+  if (env === 'production') {
+    config.plugins.push(
+      uglify({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false,
+        },
+      })
+    )
+  }
+
+  return config
 }
 
-if (env === 'production') {
-  config.plugins.push(
-    uglify({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false
-      }
-    })
-  )
-}
-
-export default config
+export default [
+  getUMDConfig({ env: 'development', file: 'dist/react-powerplug.js' }),
+  getUMDConfig({ env: 'production', file: 'dist/react-powerplug.min.js' }),
+]
