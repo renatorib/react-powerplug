833c3b94d3b82278a12a6d6d56ea089c7c7e3263
Travis Arnold <ftntravis@gmail.com>
2018-05-13 01:15:31 -0700

    Use onMouseDown instead of setTimeout to manage focus (#103)

--- src/components/FocusManager.js
+++ src/components/FocusManager.js
@@ -2,35 +2,40 @@ import * as React from 'react'
 import State from './State'
 import renderProps from '../utils/renderProps'
 
-const FocusManager = ({ onChange, ...props }) => (
-  <State
-    initial={{ isFocused: false, timeoutId: null }}
-    onChange={({ isFocused }) => onChange && onChange({ isFocused })}
-  >
-    {({ state, setState }) =>
-      renderProps(props, {
-        isFocused: state.isFocused,
-        blur: () => {
-          setState({ isFocused: false })
-        },
-        bind: {
-          tabIndex: -1,
-          onBlur: () => {
-            // the timeoutId is saved in state to not cleanup in a rerender
-            setState({
-              timeoutId: setTimeout(() => {
+const FocusManager = ({ onChange, ...props }) => {
+  let canBlur = true
+  return (
+    <State
+      initial={{ isFocused: false }}
+      onChange={({ isFocused }) => onChange && onChange({ isFocused })}
+    >
+      {({ state, setState }) =>
+        renderProps(props, {
+          isFocused: state.isFocused,
+          blur: () => {
+            setState({ isFocused: false })
+          },
+          bind: {
+            tabIndex: -1,
+            onBlur: () => {
+              if (canBlur) {
                 setState({ isFocused: false })
-              }),
-            })
+              }
+            },
+            onFocus: () => {
+              setState({ isFocused: true })
+            },
+            onMouseDown: () => {
+              canBlur = false
+            },
+            onMouseUp: () => {
+              canBlur = true
+            },
           },
-          onFocus: () => {
-            clearTimeout(state.timeoutId)
-            setState({ isFocused: true })
-          },
-        },
-      })
-    }
-  </State>
-)
+        })
+      }
+    </State>
+  )
+}
 
 export default FocusManager
