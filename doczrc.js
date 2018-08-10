6ff491bd1991993cffa2b2b4bb0e62ea8a02bb26
Pedro Nauck <pedronauck@gmail.com>
2018-08-10 19:46:16 -0300

    chore: add logo and primary color

--- doczrc.js
+++ doczrc.js
@@ -2,4 +2,14 @@ export default {
   title: 'React PowerPlug',
   description: '-',
   propsParser: false,
+  themeConfig: {
+    logo: {
+      src:
+        'https://raw.githubusercontent.com/renatorib/react-powerplug/master/logo.png',
+      width: 231,
+    },
+    colors: {
+      primary: '#3E9DE1',
+    },
+  },
 }
